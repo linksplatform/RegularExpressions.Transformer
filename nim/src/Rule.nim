1194ec6de6112679c997799ad516e89c67535a76
Ethosa <49402667+Ethosa@users.noreply.github.com>
2020-01-15 16:41:19 +0700

    code fix

--- nim/src/Rule.nim
+++ nim/src/Rule.nim
@@ -1,5 +1,6 @@
+# author: Ethosa
 import nre
-import strformat
+from strformat import fmt
 
 type
     Rule* = object
@@ -8,8 +9,16 @@ type
         file*: string
         count*: int
 
-proc newRule*(rule: Regex = re"", replace: string = "", file: string = "nil", count: int = 0): Rule =
-    return Rule(rule: rule, replace: replace, file: file, count: count)
-
 proc `$`*(r: Rule): string =
+    ## Converts Rule object to string.
     result = fmt"""("{r.rule.pattern}", "{r.replace}", "{r.file}", {r.count})"""
+
+proc newRule*(rule: Regex = re"", replace: string = "",
+              file: string = "nil", count: int = 0): Rule =
+    ## Creates new Rule object.
+    ##
+    ## Keyword Arguments:
+    ##     rule {Regex} -- pattern for rule. (default: {re""})
+    ##     replace {string} -- replace for rule. (default: {""})
+    ##     count {int} -- count for use pattern. (default: {0})
+    return Rule(rule: rule, replace: replace, file: file, count: count)
--- nim/src/retranslator.nim
+++ nim/src/retranslator.nim
@@ -1,7 +1,9 @@
+# author: Ethosa
 import nre
-import times
-import strformat
+export nre
+
 import Rule
+export Rule
 
 
 type
@@ -9,26 +11,38 @@ type
         code*: string
         rules*: seq[Rule]
 
+proc `$`*(t: Transformer): string =
+    ## Converts Transformer object in string
+    result = "Transformer with rules: " & $(t.rules)
+
 proc newTransformer*(code: string = "", rules: seq[Rule] = @[]): Transformer =
+    ## creates new Transformer object.
+    ##
+    ## Keyword Arguments:
+    ##     code {string} -- code for transform. (default: {""})
+    ##     rules {seq[Rule]} -- rules for transform. (default: {@[]})
     return Transformer(code: code, rules: rules)
 
-proc `$`*(t: Transformer): string =
-    result = fmt"Transformer with rules: {t.rules}"
-
-method Transform*(t: Transformer, code: string = ""): string {.base.} =
-    var tr = code
+proc Transform*(t: Transformer, code: string = ""): string =
+    ## transforms text, using specific rules.
+    ##
+    ## Keyword Arguments:
+    ##     code {string} -- code for transform. (default: {t.code})
+    var
+        tr = code
+        count: int = 0
     if tr == "":
         tr = t.code
 
     for i in t.rules:
-
-        var count: int = 0
+        count = i.count
         tr = tr.replace(i.rule, i.replace)
 
+        # while matched and count > 0
         while tr.find(i.rule).isSome:
             if count+1 > i.count:
                 break
-            count += 1
+            inc(count)
             tr = tr.replace(i.rule, i.replace)
 
     return tr
