a448f0ccb4c12934108deca8f8160b5f05fb2aa8
Konstantin Diachenko <drakonard@gmail.com>
2020-02-27 20:59:24 +0300

    Version 0.1.0. Performance optimizations.

--- csharp/Platform.RegularExpressions.Transformer/ISubstitutionRule.cs
+++ csharp/Platform.RegularExpressions.Transformer/ISubstitutionRule.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 
@@ -6,9 +7,28 @@ namespace Platform.RegularExpressions.Transformer
 {
     public interface ISubstitutionRule
     {
-        Regex MatchPattern { get; }
-        string SubstitutionPattern { get; }
-        Regex PathPattern { get; }
-        int MaximumRepeatCount { get; }
+        Regex MatchPattern
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get;
+        }
+
+        string SubstitutionPattern
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get;
+        }
+
+        Regex PathPattern
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get;
+        }
+
+        int MaximumRepeatCount
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get;
+        }
     }
 }
